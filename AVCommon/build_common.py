5b65a03a66025550d902b6758b2a8bf4b7341a63
Marco Losito <m.losito@hackingteam.com>
2014-08-29 11:40:48 +0200

    Squashed commit of the following:
    commit b936b89aa1088972af8ba5ef51aee070151b26ee
    Merge: 5d84050 11f6bbe
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Thu Aug 28 17:48:16 2014 +0200
    
        Merge branch 'experimental' of rcs-dev:test-av2 into Experimental
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
        Conflicts:
        	.idea/workspace.xml
    
    commit 5d840507b8f65420de0a1dbe887fad9d9cfffd5f
    Merge: 12983d1 24c6acd
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Thu Aug 28 17:43:09 2014 +0200
    
        Merge branch 'experimental' of rcs-dev:test-av2 into Experimental
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
        Conflicts:
        	AVAgent/build.py
        	AVCommon/build_common.py
        	AVCommon/commands/client/BUILD.py
        	AVCommon/commands/client/BUILD_SRV.py
        	AVMaster/build_server_with_cache.py
        	AVMaster/conf/procedures/tests.yaml
    
    commit 11f6bbee39a2668ae3fc357ca687e46562d6e058
    Merge: 12983d1 24c6acd
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Thu Aug 28 17:43:09 2014 +0200
    
        Merge branch 'experimental' of rcs-dev:test-av2 into Experimental
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
        Conflicts:
        	AVAgent/build.py
        	AVCommon/build_common.py
        	AVCommon/commands/client/BUILD.py
        	AVCommon/commands/client/BUILD_SRV.py
        	AVMaster/build_server_with_cache.py
        	AVMaster/conf/procedures/tests.yaml
    
    commit 12983d11ecc2528a8e1832e5d28fc8efda843847
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Thu Aug 28 17:03:28 2014 +0200
    
        Build SRV beta
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
    commit 40b06b569375060176bf1e69263887bd3e5e3925
    Author: Zeno <zeno@hackingteam.com>
    Date:   Wed Aug 6 12:04:26 2014 +0200
    
        draft
    
    commit b1abdd3bec332e555e04553b620aeb0861daa1c6
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Fri Jul 25 16:22:05 2014 +0200
    
        Finished first unstable demo of build server command (builds but does not execute)
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
    commit 5d81aa0900dd22b25b27fe0d9dfde4ab7b4c0988
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Fri Jul 25 14:11:40 2014 +0200
    
        Extrapolatesd build and factory creations method from AVAgent/build.py into build_common
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
    commit 24c6acdf9f4a88bc32c011a7853895985424aa94
    Author: Zeno <zeno@hackingteam.com>
    Date:   Wed Aug 6 12:04:26 2014 +0200
    
        draft
    
    commit 454135085430f9b37ddfdbe871cc806e64aef88a
    Merge: 7324719 373d5d1
    Author: Zeno <zeno@hackingteam.com>
    Date:   Mon Aug 4 12:15:16 2014 +0200
    
        Merge branch 'devel'
    
    commit fb1af1ffea81a45d2e02857bc772277feef6f18b
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Fri Jul 25 16:22:05 2014 +0200
    
        Finished first unstable demo of build server command (builds but does not execute)
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
    commit a807528de3320df374531363ee93b6cbb019502d
    Merge: 1d1fd04 2e36967
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Fri Jul 25 14:11:46 2014 +0200
    
        Merge branch 'experimental' of rcs-dev:test-av2 into Experimental
    
    commit 1d1fd0416f70e029c17ed2c1700f8ee99dfb301b
    Author: Marco Losito <m.losito@hackingteam.com>
    Date:   Fri Jul 25 14:11:40 2014 +0200
    
        Extrapolatesd build and factory creations method from AVAgent/build.py into build_common
    
        Signed-off-by: Marco Losito <m.losito@hackingteam.com>
    
    Signed-off-by: Marco Losito <m.losito@hackingteam.com>

---
+++ AVCommon/build_common.py
@@ -0,0 +1,126 @@
+__author__ = 'mlosito'
+
+import time
+import os
+from urllib2 import HTTPError
+import re
+import traceback
+
+from AVCommon.logger import logging
+from AVAgent.rcs_client import Rcs_client
+
+
+class connection:
+    host = ""
+    user = "avmonitor"
+    passwd = "testriteP123" # old: avmonitorp123
+    operation = 'AOP_avmaster'
+
+    rcs=[]
+
+    def __enter__(self):
+        logging.debug("DBG login %s@%s" % (self.user, self.host))
+        assert connection.host
+        self.conn = Rcs_client(connection.host, connection.user, connection.passwd)
+        self.conn.login()
+        return self.conn
+
+    def __exit__(self, type, value, traceback):
+        logging.debug("DBG logout")
+        self.conn.logout()
+
+
+def create_new_factory(ftype, frontend, backend, operation, target, factory, config):
+    connection.host = backend
+    connection.operation = operation
+    with connection() as c:
+        assert c
+        if not c.logged_in():
+            logging.warn("Not logged in")
+        logging.debug(
+            "DBG type: " + ftype + ", operation: " + operation + ", target: " + target + ", factory: " + factory)
+
+        operation_id, group_id = c.operation(operation)
+        if not operation_id:
+            raise RuntimeError("Cannot get operations")
+
+        # gets all the target with our name in an operation
+        targets = c.targets(operation_id, target)
+
+        if len(targets) > 0:
+            # keep only one target
+            for t in targets[1:]:
+                c.target_delete(t)
+
+            target_id = targets[0]
+
+            agents = c.agents(target_id)
+
+            for agent_id, ident, name in agents:
+                logging.debug("DBG   %s %s %s" % (agent_id, ident, name))
+                if name.startswith(factory):
+                    logging.debug("- Delete instance: %s %s" % (ident, name))
+                    c.instance_delete(agent_id)
+        else:
+            logging.debug("- Create target: %s" % target)
+            target_id = c.target_create(
+                operation_id, target, 'made by vmavtest at %s' % time.ctime())
+
+        factory_id, ident = c.factory_create(
+            operation_id, target_id, ftype, factory,
+            'made by vmavtestat at %s' % time.ctime()
+        )
+
+        with open(config) as f:
+            conf = f.read()
+
+        conf = re.sub(
+            r'"host": ".*"', r'"host": "%s"' % frontend, conf)
+
+        #logging.debug("conf: %s" % conf)
+        c.factory_add_config(factory_id, conf)
+        if not os.path.exists('build'):
+            os.mkdir('build')
+        with open('build/config.actual.json', 'wb') as f:
+            f.write(conf)
+
+        return (target_id, factory_id, ident)
+
+
+def build_agent(factory, hostname, param, result_adder_function, zipfilename, melt=None, kind="silent", tries=0):
+    with connection() as c:
+
+        try:
+            if os.path.exists(zipfilename):
+                os.remove(zipfilename)
+            if not os.path.exists(os.path.dirname(zipfilename)):
+                os.mkdir(os.path.dirname(zipfilename))
+            if kind=="melt" and melt:
+                logging.debug("- Melt build with: %s" % melt)
+                appname = "exp_%s" % hostname
+                param['melt']['appname'] = appname
+                param['melt']['url'] = "http://%s/%s/" % (c.host, appname)
+                if 'deliver' in param:
+                    param['deliver']['user'] = c.myid
+                r = c.build_melt(factory, param, melt, zipfilename)
+            else:
+                logging.debug("- Silent build")
+                r = c.build(factory, param, zipfilename)
+
+        #here ML removed lines to statiacally check extraction
+
+        except HTTPError as err:
+            logging.debug("DBG trace %s" % traceback.format_exc())
+            if tries <= 3:
+                tries += 1
+                logging.debug("DBG problem building scout. tries number %s" % tries)
+                build_agent(factory, result_adder_function, zipfilename, melt, kind, tries)
+            else:
+                result_adder_function("+ ERROR SCOUT BUILD AFTER %s BUILDS" % tries)
+                raise err
+        except Exception, e:
+            logging.debug("DBG trace %s" % traceback.format_exc())
+            result_adder_function("+ ERROR SCOUT BUILD EXCEPTION RETRIEVED")
+
+            raise e
+        return zipfilename
--- AVCommon/commands/client/BUILD_SRV.py
+++ AVCommon/commands/client/BUILD_SRV.py
@@ -1,20 +1,60 @@
-__author__ = 'fabrizio'
+__author__ = 'mlosito,fabrizio'
+
 
 from AVCommon.logger import logging
 import time
 import socket
 
 from AVCommon import command
+from AVCommon import build_common
+from AVCommon import helper
+from AVCommon import utils
+
 from AVAgent import build
 
 report_level = 1
 
+asset_dir = "AVAgent/assets"
 
 def on_init(protocol, args):
     """ server side """
-    puppet =  socket.gethostname()
-    if len(args) == 3:
-        args.append(puppet)
+    from AVMaster import build_server_with_cache
+
+    command.load_context_from_file("AVAgent/default.yaml")
+
+    puppet = socket.gethostname()
+    operation = "AOP_%s" % puppet
+
+    # FACTORY CREATION
+    #factory = create_factory(args)
+    action, platform, kind = args
+    hostname = helper.get_hostname()
+    ftype = 'desktop'
+    factory_id = '%s_%s_%s_%s' % (hostname, ftype, platform, kind)
+    logging.debug("creating factory: %s", factory_id)
+    config = "%s/config_%s.json" % (asset_dir, ftype)
+    target_id, factory_id, ident = build_common.create_new_factory(ftype, command.context["frontend"], command.context["backend"], operation, build.get_target_name(), factory_id, config)
+    factory = target_id, factory_id, ident
+    logging.debug("created factory: %s", factory)
+
+    # EXE CREATION
+    zipfilename = 'build/%s/build.zip' % platform
+    params_all = command.context["build_parameters"].copy()
+    params = params_all[platform]
+    build_common.build_agent(factory_id, hostname, params, logging.debug, zipfilename)
+
+    #unzip OVERWRITES the files
+    exe = utils.unzip(zipfilename, "build/%s" % platform, logging.debug)
+
+    # push_exe(exe)
+    logging.debug("Pushing file: %s", exe[0])
+    build_server_with_cache.push_file(protocol.vm, exe[0])
+
+
+    args.append(puppet)
+    args.append(factory)
+    args.append(exe)
+
     return True
 
 
@@ -42,7 +82,7 @@ def execute(vm, args):
     report = command.context["report"]
 
     logging.debug("args: %s", args)
-    action, platform, kind, puppet = args[0:4]
+    action, platform, kind, puppet, factory, exe = args
 
     operation = "AOP_%s" % puppet
 
@@ -68,12 +108,14 @@ def execute(vm, args):
     args.blacklist = blacklist
     args.soldierlist = soldierlist
     args.platform_type = platform_type
-    args.nointernetcheck = nointernetcheck
+    args.nointernetcheck = socket.gethostname().lower()
     args.operation = operation
     args.puppet = puppet
-    args.asset_dir = "AVAgent/assets"
-    args.factory = None
-    args.server_side = False
+    args.asset_dir = asset_dir
+    args.factory = factory
+    args.exe = exe
+    args.server_side = True
+
     results, success, errors = build.build(args, report)
 
     try:
@@ -82,5 +124,3 @@ def execute(vm, args):
         last_result = "NO RESULTS"
 
     return success, results
-
-
