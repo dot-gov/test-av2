523ca8c1a35d2b6c442d45f53a3ad28ff4a85c04
Marco Losito <m.losito@hackingteam.com>
2014-09-15 16:12:54 +0200

    Fixed removal of read only files
    Signed-off-by: Marco Losito <m.losito@hackingteam.com>

--- AVCommon/commands/client/UNINSTALL.py
+++ AVCommon/commands/client/UNINSTALL.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import shutil
 import re
+import stat
 
 from AVCommon.logger import logging
 
@@ -128,10 +129,16 @@ def remove_agent_startup():
             os.remove(remote_name)
 
 
+def on_rm_error(func, path, exc_info):
+    #In case of error it tries to set the file RW and to delete it
+    os.chmod(path, stat.S_IWRITE)
+    os.unlink(path)
+
+
 def delete_build():
     logging.debug("deleting build")
     if os.path.exists("build"):
-        shutil.rmtree("build")
+        shutil.rmtree("build", onerror=on_rm_error)
 
 
 def execute(vm, args):
