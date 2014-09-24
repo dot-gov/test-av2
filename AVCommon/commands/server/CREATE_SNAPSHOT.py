d1e3b4e5dccdce7d690e5a1aae1eaea2195c4eb4
Zeno <zeno@hackingteam.com>
2014-09-24 17:06:52 +0200

    added some commands for zeus

---
+++ AVCommon/commands/server/CREATE_SNAPSHOT.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from AVCommon.logger import logging
+
+
+def execute(vm, protocol, args):
+    """ server side """
+
+    from AVMaster import vm_manager
+
+    logging.debug("    CS Execute CREATE SNAPSHOT")
+    assert vm, "null vm"
+
+    # TODO: check
+    vm_manager.execute(vm, "createSnapshot", args)
+    return True, "Snapshot refreshed for VM"
+
