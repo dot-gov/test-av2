340224f7fee09895d8db8f3307d274ea5847e8d2
Marco Losito <m.losito@hackingteam.com>
2014-11-10 14:32:20 +0100

    Created a specific configuration for Puppet in VM_ALL
    Signed-off-by: Marco Losito <m.losito@hackingteam.com>

--- AVCommon/commands/meta/VM_ALL.py
+++ AVCommon/commands/meta/VM_ALL.py
@@ -1,7 +1,7 @@
 __author__ = 'zeno'
 
 from AVCommon.logger import logging
-import time
+import socket
 
 from AVCommon import command
 
@@ -16,8 +16,20 @@ def execute(vm, protocol, level):
     vm_first = "avast,avast32,avg,avg32,avira,avira15,kis,kis14,kis32,kis15,mcafee,norton,comodo,eset,eset7,msessential,panda,panda15".split(',')
     #disattivato temporaneamente norman
     vm_second = "drweb,360cn5,adaware,ahnlab,bitdef,bitdef15,fsecure,gdata,iobit32,vba32,fortinet,mbytes,risint,syscare,trendm,trendm15,zoneal,clamav,fprot".split(',')
-
+    # just as a documentation
+    # vm_not_working_first = "".split(',')
+    # vm_not_working_second = "norman".split(',')
     vm_ignored = ""
+
+    #in case of "puppet" host, I have different vms
+    if "avmaster" == socket.gethostname():
+        # 29
+        vm_first = "avast,avast32,avg,avg32,avg8,avira,comodo,kis14,kis,mcafee,msessential,norton,panda,kis32".split(',')
+        vm_second = "360cn,adaware,bitdef,drweb,fsecure,gdata,mbytes,norman,risint,trendm,iobit32,zoneal".split(',')
+        # just as a documentation
+        # vm_not_working_first = "eset".split(',')
+        # vm_not_working_second = "ahnlab".split(',')
+        vm_ignored = "emsisoft".split(',')
 
     if level and level.lower() == "important":
         vm_all = vm_first
