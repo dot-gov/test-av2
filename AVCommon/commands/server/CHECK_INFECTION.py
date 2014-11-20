78a8fb7c749eb82bb14fceb04c79d196227fc3ea
Marco Losito <m.losito@hackingteam.com>
2014-11-20 10:08:16 +0100

    Moved the tni files, created a new agent configuration to write a file when in elite, updated check_infection to detect elite execution, updated vm_all, fixed build. yaml
    Signed-off-by: Marco Losito <m.losito@hackingteam.com>

--- AVCommon/commands/server/CHECK_INFECTION.py
+++ AVCommon/commands/server/CHECK_INFECTION.py
@@ -18,13 +18,14 @@ def execute(vm, protocol, args):
 
     #blacklist = ['BTHSAmpPalService','CyCpIo','CyHidWin','iSCTsysTray','quickset']
 
-    dirs = ['C:Users/avtest/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup',
+    dirs = ['C:/Users/avtest/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup',
             'C:/Documents and Settings/avtest/Start Menu/Programs/Startup']
 
 
     names = build.names[:]
     names.remove("agent")
 
+    #checks scout and soldier
     for d in dirs:
         out = vm_manager.execute(vm, "listDirectoryInGuest", d)
         #logging.debug("listDirectoryInGuest: %s" % out)
@@ -34,6 +35,12 @@ def execute(vm, protocol, args):
                 logging.info("%s, found %s in %s" % (vm, b, d))
                 clean = False
                 break
+
+    #checks elite
+    out = vm_manager.execute(vm, "listDirectoryInGuest", "c:/")
+    if "infe.txt" in out:
+        logging.info("%s, ELITE detected: found infe.txt in c:" % vm)
+        clean = False
 
     ret = xor(clean is True, invert)
     if clean is True:
