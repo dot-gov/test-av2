2e1487232474b87865ba71f58ae654a42e40831b
olli <olli@hackingteam.it>
2014-12-05 18:08:41 +0100

    avg internet security 2015 added to automatic tests

--- AVCommon/commands/meta/VM_ALL.py
+++ AVCommon/commands/meta/VM_ALL.py
@@ -13,7 +13,7 @@ def execute(vm, protocol, level):
     assert command.context is not None
 
     #vm_first = "avast,avast32,avg,avg32,avira,kis,kis14,kis32,mcafee,norton,panda,comodo,eset,msessential".split(',')
-    vm_first = "eset7,avast,avast32,avg,panda15,avg32,avira,avira15,kis15,kis14,kis32,mcafee,bitdef15,norton,comodo,eset,msessential,panda".split(',')
+    vm_first = "eset7,avast,avast32,avg,avg15,panda15,avg32,avira,avira15,kis15,kis14,kis32,mcafee,bitdef15,norton,comodo,eset,msessential,panda".split(',')
     #disattivato temporaneamente norman
     #disattivato DEFINITIVAMENTE kis e ahnlab (kis e' un kis 2013 che ormai non si usa piu', ahnlab e' scaduto e non si riesce a riattivare)
     vm_second = "fortinet,drweb,360cn5,adaware,fprot,bitdef,fsecure,gdata,vba32,iobit32,mbytes,risint,syscare,trendm,trendm15,zoneal,clamav".split(',')
