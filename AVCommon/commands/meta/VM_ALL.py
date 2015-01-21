08253e0eb9fa7d4508b458e609b4b7cffc4ac999
Marco Losito <m.losito@hackingteam.com>
2015-01-21 17:00:26 +0100

    Added Norton and 360ts

--- AVCommon/commands/meta/VM_ALL.py
+++ AVCommon/commands/meta/VM_ALL.py
@@ -13,10 +13,10 @@ def execute(vm, protocol, level):
     assert command.context is not None
 
     #vm_first = "avast,avast32,avg,avg32,avira,kis,kis14,kis32,mcafee,norton,panda,comodo,eset,msessential".split(',')
-    vm_first = "eset7,avast,avast32,avg,avg15,panda15,avg32,avira,avira15,kis15,kis14,kis32,mcafee,bitdef15,norton,comodo,eset,msessential,panda".split(',')
+    vm_first = "eset7,avast,avast32,avg,avg15,panda15,avg32,avira,avira15,kis15,kis14,kis32,mcafee,bitdef15,norton,comodo,eset,msessential,panda,norton15".split(',')
     #disattivato temporaneamente norman
     #disattivato DEFINITIVAMENTE kis e ahnlab (kis e' un kis 2013 che ormai non si usa piu', ahnlab e' scaduto e non si riesce a riattivare)
-    vm_second = "fortinet,drweb,360cn5,cmcav,adaware,fprot,bitdef,fsecure,gdata,vba32,iobit32,mbytes,risint,syscare,trendm,trendm15,zoneal,clamav".split(',')
+    vm_second = "fortinet,drweb,360cn5,cmcav,adaware,fprot,bitdef,fsecure,gdata,vba32,iobit32,mbytes,risint,syscare,trendm,trendm15,zoneal,clamav,360ts".split(',')
     # just as a documentation
     # vm_not_working_first = "".split(',')
     # vm_not_working_second = "norman".split(',')
